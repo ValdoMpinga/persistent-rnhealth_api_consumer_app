9951b7858331144d4a7a3f0e6cc06aea9a6a640f
Valdo Mpinga <valdompinga2@gmail.com>
2023-05-09 10:06:39 +0100

    code refactored to a class based approach

--- src/script.ts
+++ src/script.ts
@@ -13,120 +13,130 @@ interface MeasurementRequestBody {
   groupBy: string;
 }
 
-const app = express();
-const TWENTY_HOURS_IN_MILLISECONDS = 72000000;
-const SENSORS = ["D001", "D003"];
-const RN_MONITOR_API = axios.create({
-  baseURL: "http://62.48.168.89:3000/api",
-});
+class PersistentRnHealthConsumerApp {
+  private app: express.Application;
+  private TWENTY_HOURS_IN_MILLISECONDS: number = 72000000;
+  private SENSORS: string[] = ["D001", "D003"];
+  private RN_MONITOR_API = axios.create({
+    baseURL: "http://62.48.168.89:3000/api",
+  });
+  private RN_HEALTH_FORECASTER_API = axios.create({
+    baseURL: "http://localhost:8000",
+  });
+  private isFirstForecast: boolean = true;
+  private sixHoursInterval: number = 6 * 60 * 60 * 1000;
+  private threeMinutesInterval: number = 2 * 60 * 1000;
+  private intervalCounter: number = 0;
 
-const RN_HEALTH_FORECASTER_API = axios.create({
-  baseURL: "http://localhost:8000",
-});
-let isFirstForecast = true;
-const sixHoursInterval = 6 * 60 * 60 * 1000;
-const threeMinutesInterval = 2 * 60 * 1000;
-let intervalCounter = 0;
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-function filterMeasurement(measurement) {
-  return (
-    measurement.T != null &&
-    measurement.Rn != null &&
-    measurement.H != null &&
-    measurement.P != null &&
-    measurement.CO2 != null &&
-    measurement.sensor_id === SENSORS[0]
-  );
-}
-
-async function fetchDataAndForecast() {
-  console.log("In");
-  console.log("Interval counter: " + intervalCounter);
-  intervalCounter++;
-
-  const date = dayjs();
-  const parsedCurrentDate = date.toDate().getTime().toString();
-  const parsedStartDate = (
-    date.toDate().getTime() - TWENTY_HOURS_IN_MILLISECONDS
-  ).toString();
-  const requestBody: MeasurementRequestBody = {
-    dateStart: parsedStartDate,
-    dateEnd: parsedCurrentDate,
-    groupBy: "1h",
-  };
-  try {
-    const response = await RN_MONITOR_API.post(
-      "/measurement/get",
-      requestBody,
-      {
-        headers: {
-          Authorization: process.env.RN_MONITOR_KEY,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const filteredData = response.data
-      .filter(filterMeasurement)
-      .map((measure) => {
-        delete measure.sensor_id;
-        return measure;
-      });
-    // console.log(filteredData);
-    await RN_HEALTH_FORECASTER_API.post("/forecast/target-sensor", {
-      targetSensor: SENSORS[0],
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.listen(process.env.PORT || 3000, () => {
+      console.log("Server is running on port 3000");
+      this.initialize();
     });
+  }
 
-    console.log("Getting LSTM forecasts");
-    setTimeout(() => {}, 1000);
-    const lstmResponse = await RN_HEALTH_FORECASTER_API.post(
-      "/forecast/lstm",
-      filteredData
+  private filterMeasurement(measurement) {
+    return (
+      measurement.T != null &&
+      measurement.Rn != null &&
+      measurement.H != null &&
+      measurement.P != null &&
+      measurement.CO2 != null &&
+      measurement.sensor_id === this.SENSORS[0]
     );
-    console.log(lstmResponse.data);
+  }
 
-    // await saveForecastsToDB();
-    console.log("Getting Bi-LSTM forecasts");
-    setTimeout(() => {}, 1000);
+  private async fetchDataAndForecast() {
+    console.log("Interval counter: " + this.intervalCounter);
+    this.intervalCounter++;
 
-    const biLstmResponse = await RN_HEALTH_FORECASTER_API.post(
-      "/forecast/bi-lstm",
-      filteredData
-    );
-    console.log(biLstmResponse.data);
+    const date = dayjs();
+    const parsedCurrentDate = date.toDate().getTime().toString();
+    const parsedStartDate = (
+      date.toDate().getTime() - this.TWENTY_HOURS_IN_MILLISECONDS
+    ).toString();
+    const requestBody: MeasurementRequestBody = {
+      dateStart: parsedStartDate,
+      dateEnd: parsedCurrentDate,
+      groupBy: "1h",
+    };
 
-    await saveForecastsToDB();
-    if (isFirstForecast) {
-      isFirstForecast = false;
-    } else {
-      await saveMeasumentsToDB();
+    try {
+      const response = await this.RN_MONITOR_API.post(
+        "/measurement/get",
+        requestBody,
+        {
+          headers: {
+            Authorization: process.env.RN_MONITOR_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const filteredData = response.data
+        .filter(this.filterMeasurement.bind(this))
+        .map((measure) => {
+          delete measure.sensor_id;
+          return measure;
+        });
+
+      await this.RN_HEALTH_FORECASTER_API.post("/forecast/target-sensor", {
+        targetSensor: this.SENSORS[0],
+      });
+
+      console.log("Getting LSTM forecasts");
+      setTimeout(() => {}, 1000);
+      const lstmResponse = await this.RN_HEALTH_FORECASTER_API.post(
+        "/forecast/lstm",
+        filteredData
+      );
+      console.log(lstmResponse.data);
+
+      console.log("Getting Bi-LSTM forecasts");
+      setTimeout(() => {}, 1000);
+      const biLstmResponse = await this.RN_HEALTH_FORECASTER_API.post(
+        "/forecast/bi-lstm",
+        filteredData
+      );
+      console.log(biLstmResponse.data);
+
+      await this.saveForecastsToDB();
+      if (this.isFirstForecast) {
+        this.isFirstForecast = false;
+      } else {
+        await this.saveMeasumentsToDB();
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-  } catch (error) {
-    console.error(error);
+  private async saveForecastsToDB() {
+    console.log("forecasts saved to database");
+  }
+
+  private async saveMeasumentsToDB() {
+    console.log("measurements saved to database");
+  }
+
+  private async initialize() {
+    setTimeout(async () => {
+      try {
+        await this.fetchDataAndForecast();
+
+        setInterval(async () => {
+          await this.fetchDataAndForecast();
+          console.log("Forecasts re-scheduled to run in 6 hours");
+        }, this.threeMinutesInterval);
+      } catch (e) {
+        console.log(e);
+      }
+    }, 0);
   }
 }
 
-setTimeout(() => {
-  try {
-    fetchDataAndForecast();
-
-    setInterval(() => {
-      console.log("Forecasts re-schaduled to run in 6 hours");
-      fetchDataAndForecast();
-    }, threeMinutesInterval);
-  } catch (e) {
-    console.log(e);
-  }
-}, 0);
-
-async function saveForecastsToDB() {
-  console.log("forecasts saved to database");
-}
-
-async function saveMeasumentsToDB() {
-  console.log("measurements saved to database");
-}
+new PersistentRnHealthConsumerApp();
